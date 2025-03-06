509394f2650654cdefc863798cea44f54c60247e
Théo <theo.arbogast@hotmail.fr>
2025-03-06 16:58:25 +0100

    update/feat : préparation de récupérer d'API Youtube

--- assets/script.js
+++ assets/script.js
@@ -70,3 +70,19 @@ openModal.forEach(opMo => {
         }
     })
 });
+
+
+
+// const subNumber = document.createElement("a")
+
+// async function call() {
+    
+//     let IDchannel = "UC2FAM-zL-PhH0OkIT95aWJQ"
+
+//     let response = await fetch(
+//         `https://www.googleapis.com/youtube/v3/channels?id=${window.apiKey}&key=${APIkey}&part=statistics&fields=items/statistics`)
+//     let data = await response.json()
+//     console.log(data)
+// }
+
+// call()

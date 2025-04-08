506432fb1a357dc346d6c81c82df90de7b9b5ad4
Théo <theo.arbogast@hotmail.fr>
2025-04-08 17:02:11 +0200

    update : préparation du Home controller pour les pages du footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,4 +17,28 @@ final class HomeController extends AbstractController
             'controller_name' => 'HomeController'
         ]);
     }
+
+    #[Route('/about', name: 'app_about')]
+    public function indexAbout()
+    {
+
+        return $this->render('home/about.html.twig', [
+        ]);
+    }
+
+    #[Route('/legal', name: 'app_legal')]
+    public function indexLegal()
+    {
+
+        return $this->render('home/legal.html.twig', [
+        ]);
+    }
+
+    #[Route('/privacy', name: 'app_privacy')]
+    public function indexPrivacy()
+    {
+
+        return $this->render('home/privacy.html.twig', [
+        ]);
+    }
 }
